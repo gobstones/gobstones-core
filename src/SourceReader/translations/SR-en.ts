872c491d323c5ff2488015eb83887ac8b2664ed1
fidel <fidel@unq.edu.ar>
2023-02-02 10:55:13 -0300

    Intl strings trimmed of non used ones. Tests pending

--- src/SourceReader/translations/SR-en.ts
+++ src/SourceReader/translations/SR-en.ts
@@ -5,15 +5,8 @@ import { SourceReaderLocale } from './SR-Locale';
  */
 export const en: SourceReaderLocale = {
     error: {
-        WrongArgsForSourcePos: 'Invalid creation of source position.',
         NoInput: 'Cannot create a source reader with no input strings.',
         IncompatibleSilentSkip: 'Skip cannot be silent if getPos was used on this character.',
-        CannotHappenBy:
-            'Operation `${operation}` of `${context}` reached a code that cannot happen.',
-        InvariantViolationBy:
-            'Class invariant has been violated, detected by `${operation}` of `${context}`',
-        InvalidOperationForUnknownBy:
-            'Operation `${operation}` of `${context}` is not valid for unknown positions.',
         UnmatchingPositionsBy:
             'Positions determine different source inputs at `${operation}` of `${context}`.',
         AtEOFBy:
