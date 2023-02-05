fcbde71f4b2153b17cf6a9c901543f6e81f3cc54
fidel <fidel@unq.edu.ar>
2023-02-05 10:55:06 -0300

    SourceReader, complete (all tests, all docs)

--- src/SourceReader/translations/SR-es.ts
+++ src/SourceReader/translations/SR-es.ts
@@ -1,13 +1,17 @@
+/**
+ * @author Pablo E. --Fidel-- Martínez López, <fidel.ml@gmail.com>
+ * @module SourceReader
+ */
 import { SourceReaderLocale } from './SR-Locale';
 
 /**
  * The locale for Spanish language, for {@link SourceReader}.
+ * It implements interface {@link SourceReaderLocale}.
+ * @group Implementation: Translations
  */
 export const es: SourceReaderLocale = {
     error: {
         NoInput: 'No se puede crear un lector de fuentes de entrada sin cadenas de entrada.',
-        IncompatibleSilentSkip:
-            'Skip no puede hacerse en silencio si se utilizó getPos en este  caracter.',
         UnmatchingPositionsBy:
             // eslint-disable-next-line max-len
             'Las posiciones determinan fuentes de entrada diferentes en ${operation} de ${context}.',
