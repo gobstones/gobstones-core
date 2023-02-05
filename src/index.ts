e4934095258a9fc4fa6329d5cd7ca3e663c9d10e
fidel <fidel@unq.edu.ar>
2023-02-05 11:04:59 -0300

    Adding SourceReader to the module

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ export * from './Expectations';
 export * from './Translations';
 export * from './Events/EventEmitter';
 export * from './Regexp';
+export * from './SourceReader';
 export * from './helpers';
