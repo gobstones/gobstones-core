327767a3e08b8787b1f32f85a619e39df8912332
fidel <fidel@unq.edu.ar>
2023-02-09 10:22:53 -0300

    Documentation generated for deepStringAssign

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,8 +1,8 @@
 /**
- * This module provides different utilities that are used throughout the
- * {@link gobstones-core} package, but that can be reused in other packages.
- * Utilities that are widely used in different packages should be added here
- * for better accessibility in all packages.
+ * This module provides different utilities that are used throughout the `gobstones-core`
+ * package or other packages of the `GobstonesWeb2` project.
+ * Utilities with general behavior or that are widely used in different packages should
+ * be added here for better accessibility in all packages.
  *
  * @module Utils
  * @author Alan Rodas Bonjour <alanrodas@gmail.com>
